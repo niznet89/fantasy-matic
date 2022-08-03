8d0bedcc32671a4cd3f9276b974231cebf427840
Tenzin Rose <tenzinrose@gmail.com>
2022-08-03 13:23:39 -0300

    Figured out CORS issue

--- client/index.js
+++ client/index.js
@@ -1,14 +1,25 @@
 import "./index.scss";
 
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
+  const proxyURL = 'https://cors-anywhere.herokuapp.com/';
+  const baseURL = 'https://fantasy.premierleague.com/api/';
+  const api = 'https://api.allorigins.ml/raw?url=';
 
-  fetch("https://fantasy.premierleague.com/api/leagues-classic/583326/standings", {mode: 'no-cors', headers: {
-    'Content-Type': 'application/json'
-    // 'Content-Type': 'application/x-www-form-urlencoded',
-  }}).then((response) => {
-        return response;
-      }).then((data) => {
-        console.log(data);
+  // const doCORSRequest = async (url) => {
+  //   const response = await fetch(api + encodeURIComponent(baseURL + url));
+  //   const myJson = await response.json();
+  //   return myJson
+  // }
+  const test = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://fantasy.premierleague.com/api/leagues-classic/583326/standings/')}`)
+      .then(response => {
+        if (response.ok) return response.json()
+        throw new Error('Network response was not ok.')
       })
+      .then(data => {return JSON.parse(data.contents)});
+
+  // let test = await doCORSRequest("leagues-classic/583326/standings/").then((data) => {return data.standings.results});
+  console.log(test);
+
+  // console.log(test);
 });
