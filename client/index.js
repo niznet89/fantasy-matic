fb2d89dc7efa85ff3d4ec63e591a43e6eea6b661
Tenzin Rose <tenzinrose@gmail.com>
2022-08-03 17:24:49 -0300

    FEAT: Added functions on FE to build table (dummy data), & connect to the blockchain

--- client/index.js
+++ client/index.js
@@ -1,8 +1,9 @@
 import "./index.scss";
+import bootstrap from 'bootstrap';
+import { ethers } from "ethers";
 
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const proxyURL = 'https://cors-anywhere.herokuapp.com/';
   const baseURL = 'https://fantasy.premierleague.com/api/';
   const api = 'https://api.allorigins.ml/raw?url=';
 
@@ -11,15 +12,55 @@ document.addEventListener("DOMContentLoaded", async () => {
   //   const myJson = await response.json();
   //   return myJson
   // }
-  const test = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://fantasy.premierleague.com/api/leagues-classic/583326/standings/')}`)
+  const jsonObject = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://fantasy.premierleague.com/api/leagues-classic/583326/standings/')}`)
       .then(response => {
         if (response.ok) return response.json()
         throw new Error('Network response was not ok.')
       })
       .then(data => {return JSON.parse(data.contents)});
 
-  // let test = await doCORSRequest("leagues-classic/583326/standings/").then((data) => {return data.standings.results});
-  console.log(test);
+  const returnBoughtInPlayers = async(teamName) => {
+    const defaultProvider = await ethers.getDefaultProvider();
+    const fantasyContract = new ethers.Contract(address, abi, defaultProvider);
+    // Hit Function to return Array of peeps who HAVE bought in
+    const arrayOfBoughtIn;
+    if (arrayOfBoughtIn.includes(teamName)) {
+      return "";
+    } else {
+      return `<a class="btn btn-outline-danger" onClick=${clickToBuy(teamName)} href="#" role="button">Buy In</a>`
+    }
+  }
 
-  // console.log(test);
+  function clickToBuy(teamName) {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    // Need the address for contract & ABI
+    const contract = new ethers.Contract(address, abi, provider);
+    // Hit contract function to deposit
+  }
+  returnBoughtInPlayers();
+
+  console.log(jsonObject.new_entries.results);
+  const testArray = ["Put My Willian", "Crunch"];
+
+  for (let i = 0; i < testArray.length; i++) {
+    document.getElementById("standings-table").insertAdjacentHTML('beforeend', `<tr>
+      <td>${i + 1}</td>
+      <td>${returnFullname(testArray[i])}</td>
+      <td>${testArray[i]}</td>
+      <td>${"hi"}</td>
+    </tr>`);
+  }
+
+  function returnFullname(teamName) {
+    const resultsArray = jsonObject.new_entries.results;
+    let fullName;
+    for (let i = 0; i < resultsArray.length; i++) {
+      if (resultsArray[i].entry_name === teamName) {
+        fullName = resultsArray[i].player_first_name + " " + resultsArray[i].player_last_name;
+      }
+    }
+    return fullName;
+  }
 });
